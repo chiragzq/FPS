91cace32c177acfa8e4d88d666e6afba8323cc8d
Chirag Kaushik <22chiragk@students.harker.org>
2020-07-01 00:04:39 -0600

    Movement, looking, stairs

---
+++ Assets/PlayerLook.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour {
+    float INPUT_MULTIPLIER = 180.0f;
+    float xRotation = 0.0f;
+
+    public Transform playerTransform;
+
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update() {
+        float mouseX = Input.GetAxis("Mouse X") * INPUT_MULTIPLIER * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * INPUT_MULTIPLIER * Time.deltaTime;
+
+        xRotation = Mathf.Clamp(xRotation - mouseY, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerTransform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public CharacterController controller;
+
+    float SPEED_MULTIPLIER = 16f;
+
+    // Update is called once per frame
+    void Update() {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 delta = transform.right * x + transform.forward * z;
+        controller.Move(delta * SPEED_MULTIPLIER * Time.deltaTime);
+    }
+}
